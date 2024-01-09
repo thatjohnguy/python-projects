2e14be6a666362d01caaf57826bb12565b7b8814
John Omoluabi <johnomoluabimail@gmail.com>
2024-01-09 19:03:45 +0100

    concluded simple calculator

--- calculator.py
+++ calculator.py
@@ -16,4 +16,41 @@
 # 5. persist program until user wants to exit
 
 def add(a,b):
-    return f""
+    return f"{a} + {b} = {int(a) + int(b)}"
+def subtr(a,b):
+    return f"{a} - {b} = {int(a) - int(b)}"
+def mult(a,b):
+    return f"{a} x {b} = {int(a) * int(b)}"
+def divi(a,b):
+    return f"{a} / {b} = {int(a) / int(b)}"
+
+print("A - Addition")
+print("B - Subtraction")
+print("C - Multiplication")
+print("D - Division")
+
+
+while True:
+    choice=input("What Operation would you like to do?(A-D)?")
+    choice=choice.lower()
+    match choice:
+        case "a":
+            a=input("first operand: ")
+            b=input("second operand: ")
+            print(add(a,b))
+            
+        case "b":
+            a=input("first operand: ")
+            b=input("second operand: ")
+            print(subtr(a,b))
+            
+        case "c":
+            a=input("first operand: ")
+            b=input("second operand: ")
+            print(mult(a,b))
+            
+        case "d":
+            a=input("first operand: ")
+            b=input("second operand: ")
+            print(divi(a,b))
+            
