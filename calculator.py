08b877f35111773dd496ae823dbe247171210e6b
John Omoluabi <johnomoluabimail@gmail.com>
2024-01-09 15:58:02 +0100

    just started calculator app

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#  _______  _______  ___      _______  __   __  ___      _______  _______  _______  ______   
+# |       ||   _   ||   |    |       ||  | |  ||   |    |   _   ||       ||       ||    _ |  
+# |       ||  |_|  ||   |    |       ||  | |  ||   |    |  |_|  ||_     _||   _   ||   | ||  
+# |       ||       ||   |    |       ||  |_|  ||   |    |       |  |   |  |  | |  ||   |_||_ 
+# |      _||       ||   |___ |      _||       ||   |___ |       |  |   |  |  |_|  ||    __  |
+# |     |_ |   _   ||       ||     |_ |       ||       ||   _   |  |   |  |       ||   |  | |
+# |_______||__| |__||_______||_______||_______||_______||__| |__|  |___|  |_______||___|  |_|
+# John Omoluabi
+
+# checklist
+# ***********
+# 1. define functions needed; add, subtract, multiply, divide
+# 2. print options to user
+# 3. ask for values
+# 4. call functions
+# 5. persist program until user wants to exit
+
+def add(a,b):
+    return f""
